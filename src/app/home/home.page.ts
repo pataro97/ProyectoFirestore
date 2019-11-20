ad7bdfe940a05f67a90de29e0176829ba40faf47
pataro97 <alum.pablo.tamayo.romero@ieslosremedios.org>
2019-11-20 13:14:02 +0100

    bbbb

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,75 @@
 import { Component } from '@angular/core';
 
+import { FirestoreService } from '../firestore.service';
+import { Pelicula } from '../pelicula';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  peliculaEditando: Pelicula;
 
-  constructor() {}
+  arrayColeccionPeliculas: any = [{
+    id: "",
+    data: {} as Pelicula
+   }];
+
+  constructor(private firestoreService: FirestoreService) {
+    // Crear una pelicula vacía
+    this.peliculaEditando = {} as Pelicula;
+    this.obtenerListaPeliculas();
+  }
+
+  obtenerListaPeliculas(){
+    this.firestoreService.consultar("pelicula").subscribe((resultadoConsultaPeliculas) => {
+      this.arrayColeccionPeliculas = [];
+      resultadoConsultaPeliculas.forEach((datosPelicula: any) => {
+        this.arrayColeccionPeliculas.push({
+          id: datosPelicula.payload.doc.id,
+          data: datosPelicula.payload.doc.data()
+        });
+      })
+    });
+  }
+  clicBotonInsertar() {
+    this.firestoreService.insertar("pelicula", this.peliculaEditando).then(() => {
+      console.log('Pelicula creada correctamente!');
+      this.peliculaEditando= {} as Pelicula;
+    }, (error) => {
+      console.error(error);
+    });
+  }
+
+
+
+  idPeliculaSelec: string;
+
+  selecPelicula(peliculaSelec) {
+    console.log("Pelicula seleccionada: ");
+    console.log(peliculaSelec);
+    this.idPeliculaSelec = peliculaSelec.id;
+    this.peliculaEditando.titulo = peliculaSelec.data.titulo;
+    this.peliculaEditando.descripcion = peliculaSelec.data.descripcion;
+  }
+
+  clicBotonBorrar() {
+    this.firestoreService.borrar("pelicula", this.idPeliculaSelec).then(() => {
+      // Actualizar la lista completa
+      this.obtenerListaPeliculas();
+      // Limpiar datos de pantalla
+      this.peliculaEditando = {} as Pelicula;
+    })
+  }
+  
+  clicBotonModificar() {
+    this.firestoreService.actualizar("pelicula", this.idPeliculaSelec, this.peliculaEditando).then(() => {
+      // Actualizar la lista completa
+      this.obtenerListaPeliculas();
+      // Limpiar datos de pantalla
+      this.peliculaEditando = {} as Pelicula;
+    })
+  }
 
 }
