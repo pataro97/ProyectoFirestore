5a96eef5bd6b33c244c7a8e0063bd6ba051ec03d
pataro97 <alum.pablo.tamayo.romero@ieslosremedios.org>
2019-11-27 14:58:36 +0100

    hhh

--- src/app/editar/editar.page.ts
+++ src/app/editar/editar.page.ts
@@ -2,21 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
 import { Pelicula } from '../pelicula';
 import { FirestoreService } from '../firestore.service';
-
+import { Router } from "@angular/router";
 @Component({
   selector: 'app-editar',
   templateUrl: './editar.page.html',
   styleUrls: ['./editar.page.scss'],
 })
 export class EditarPage implements OnInit {
-
   document: any = {
     id: "",
     data: {} as Pelicula
   };
 
   id = null;
-  constructor(private activatedRoute: ActivatedRoute, private firestoreService: FirestoreService) { 
+  constructor(private activatedRoute: ActivatedRoute, private firestoreService: FirestoreService, private router: Router) { 
     this.firestoreService.consultarPorId("pelicula", this.activatedRoute.snapshot.paramMap.get("id")).subscribe((resultado) => {
       // Preguntar si se hay encontrado un document con ese ID
       if(resultado.payload.data() != null) {
@@ -28,6 +27,10 @@ export class EditarPage implements OnInit {
         // No se ha encontrado un document con ese ID. Vaciar los datos que hubiera
         this.document.data = {} as Pelicula;
       } 
+      if (this.id == "Nuevo"){
+        document.getElementById("modificar").innerHTML = "Añadir";
+        document.getElementById("borrar").setAttribute("class", "invisible");
+      }
     });
   }
 
@@ -35,4 +38,51 @@ export class EditarPage implements OnInit {
     this.id = this.activatedRoute.snapshot.paramMap.get("id");
   }
   
+  
+  obtenerListaPeliculas(){
+    this.firestoreService.consultar("pelicula").subscribe((resultadoConsultaPeliculas) => {
+      this.document = [];
+      resultadoConsultaPeliculas.forEach((datosPelicula: any) => {
+        this.document.push({
+          id: datosPelicula.payload.doc.id,
+          data: datosPelicula.payload.doc.data()
+        });
+      })
+    });
+  }
+
+  clicBotonBorrar() {
+    this.firestoreService.borrar("pelicula", this.document.id).then(() => {
+      // Actualizar la lista completa
+      this.obtenerListaPeliculas();
+      // Limpiar datos de pantalla
+      this.document.data = {} as Pelicula;
+      //Volver a la pagina principal
+      this.router.navigate(["/home"]);
+    })
+  }
+  
+  clicBotonModificar() {
+    if (this.id == "Nuevo") {
+        this.firestoreService.insertar("pelicula", this.document.data).then(() => {
+          console.log('Pelicula creada correctamente!');
+          this.document.data= {} as Pelicula;
+        }, (error) => {
+          console.error(error);
+        });
+        //Volver a la pagina principal
+        this.router.navigate(["/home"]);
+    }else{
+    this.firestoreService.actualizar("pelicula", this.document.id, this.document.data).then(() => {
+      // Actualizar la lista completa
+      this.obtenerListaPeliculas();
+      // Limpiar datos de pantalla
+      this.document.data = {} as Pelicula;
+      //Volver a la pagina principal
+      this.router.navigate(["/home"]);
+    })
+  }
+  }
+
+
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -37,13 +37,8 @@ export class HomePage {
       })
     });
   }
-  clicBotonInsertar() {
-    this.firestoreService.insertar("pelicula", this.peliculaEditando).then(() => {
-      console.log('Pelicula creada correctamente!');
-      this.peliculaEditando= {} as Pelicula;
-    }, (error) => {
-      console.error(error);
-    });
+  clicNuevo() {
+    this.router.navigate(["/editar/Nuevo"]);
   }
 
 
@@ -56,23 +51,23 @@ export class HomePage {
     this.peliculaEditando.descripcion = peliculaSelec.data.descripcion;
   }
 
-  clicBotonBorrar() {
-    this.firestoreService.borrar("pelicula", this.idPeliculaSelec).then(() => {
-      // Actualizar la lista completa
-      this.obtenerListaPeliculas();
-      // Limpiar datos de pantalla
-      this.peliculaEditando = {} as Pelicula;
-    })
-  }
+  // clicBotonBorrar() {
+  //   this.firestoreService.borrar("pelicula", this.idPeliculaSelec).then(() => {
+  //     // Actualizar la lista completa
+  //     this.obtenerListaPeliculas();
+  //     // Limpiar datos de pantalla
+  //     this.peliculaEditando = {} as Pelicula;
+  //   })
+  // }
   
-  clicBotonModificar() {
-    this.firestoreService.actualizar("pelicula", this.idPeliculaSelec, this.peliculaEditando).then(() => {
-      // Actualizar la lista completa
-      this.obtenerListaPeliculas();
-      // Limpiar datos de pantalla
-      this.peliculaEditando = {} as Pelicula;
-    })
-  }
+  // clicBotonModificar() {
+  //   this.firestoreService.actualizar("pelicula", this.idPeliculaSelec, this.peliculaEditando).then(() => {
+  //     // Actualizar la lista completa
+  //     this.obtenerListaPeliculas();
+  //     // Limpiar datos de pantalla
+  //     this.peliculaEditando = {} as Pelicula;
+  //   })
+  // }
 
   navigateToEditar() {
     this.router.navigate(["/editar/"+this.idPeliculaSelec]);
